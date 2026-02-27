e0c16aadb9005ea003202c9728c3b544e42dcf06
iamrichardD <richard@iamrichardD.com>
2026-02-27 08:27:03 -0800

    chore: remove build artifacts from tracking

--- website/.astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/docs/architecture.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Farchitecture.mdx&astroContentModuleFlag=true")],
-["src/content/docs/howto.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fhowto.mdx&astroContentModuleFlag=true")],
-["src/content/docs/integrations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintegrations.mdx&astroContentModuleFlag=true")],
-["src/content/docs/showcase.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fshowcase.mdx&astroContentModuleFlag=true")]]);
-		
